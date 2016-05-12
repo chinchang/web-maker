c1ef1064698acf7b91d460ff7b9a2ea5529f5574
Kushagra Gour <chinchang457@gmail.com>
2016-05-13 02:30:25 +0530

    remove tab permission.

--- src/eventPage.js
+++ src/eventPage.js
@@ -1,22 +1,3 @@
-chrome.tabs.onCreated.addListener(function (tab) {
-
-	console.log('created', arguments)
-	if (tab.url === 'chrome://newtab/') {
-		chrome.tabs.update(tab.id, {
-			url: chrome.extension.getURL('index.html')
-		}, function callback() {
-			console.log('ho gaya');
-		});
-	}
-
-});
-
-chrome.tabs.onUpdated.addListener(function () {
-
-	console.log('updated', arguments)
-
-});
-
 chrome.browserAction.onClicked.addListener(function(activeTab) {
     chrome.tabs.create({ url: chrome.extension.getURL('index.html'), selected: true });
 });

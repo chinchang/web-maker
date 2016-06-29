c87aee8bb998531023f78e6bbb4101ab7b7ca4c4
Kushagra Gour <chinchang457@gmail.com>
2016-06-30 03:51:36 +0530

    Revert "remove tab permission."
    This reverts commit c1ef1064698acf7b91d460ff7b9a2ea5529f5574.
    
    Conflicts:
    	src/eventPage.js
    	src/manifest.json

--- src/eventPage.js
+++ src/eventPage.js
@@ -4,3 +4,18 @@ chrome.browserAction.onClicked.addListener(function(){
 		selected: true
 	});
 });
+
+chrome.tabs.onCreated.addListener(function (tab) {
+	console.log('created', arguments)
+	if (tab.url === 'chrome://newtab/') {
+		chrome.tabs.update(tab.id, {
+			url: chrome.extension.getURL('index.html')
+		}, function callback() {
+			console.log('ho gaya');
+		});
+	}
+});
+
+chrome.tabs.onUpdated.addListener(function () {
+	console.log('updated', arguments)
+});

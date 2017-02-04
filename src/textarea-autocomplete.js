cd583c35c37ab97f0bb6bcb332787b222e47b609
Kushagra Gour <chinchang457@gmail.com>
2017-02-05 04:11:50 +0530

    add loader and polish library autocompletion

--- src/textarea-autocomplete.js
+++ src/textarea-autocomplete.js
@@ -19,9 +19,25 @@
 			this.list.classList.add('autocomplete-dropdown');
 			wrap.insertBefore(this.list, null);
 
+			this.loader = document.createElement('div');
+			this.loader.classList.add('loader');
+			this.loader.classList.add('autocomplete__loader');
+			this.loader.style.display = 'none';
+			wrap.insertBefore(this.loader, null);
+
+			// after list is insrted into the DOM, we put it in the body
+			// fixed at same position
+			setTimeout(() => {
+				requestIdleCallback(() => {
+					document.body.appendChild(this.list);
+					this.list.style.position = 'fixed';
+				});
+			},100);
+
 			this.t.addEventListener('input', e => this.onInput(e));
 			this.t.addEventListener('keydown', e => this.onKeyDown(e));
-			this.t.addEventListener('blur', e => this.closeSuggestions())
+			this.t.addEventListener('blur', e => this.closeSuggestions());
+			this.list.addEventListener('mousedown', e => this.onListMouseDown(e));
 		}
 
 		get currentLineNumber() {
@@ -32,7 +48,7 @@
 			return this.t.value.split('\n')[line - 1];
 		}
 		closeSuggestions() {
-			this.list.parentElement.classList.remove('open');
+			this.list.classList.remove('is-open');
 			this.isShowingSuggestions = false;
 		}
 		getList(input) {
@@ -63,9 +79,12 @@
 		onInput() {
 			var currentLine = this.currentLine;
 			if (currentLine) {
+				if (currentLine.indexOf('/') !== -1 || currentLine.match(/https*:\/\//)) { return; }
 				clearTimeout(this.timeout);
 				this.timeout = setTimeout(() => {
+					this.loader.style.display = 'block';
 					this.getList(currentLine).then(arr => {
+						this.loader.style.display = 'none';
 						if (!arr.length) {
 							this.closeSuggestions();
 							return;
@@ -77,13 +96,21 @@
 						for (var i = 0; i < Math.min(arr.length, 10); i++) {
 							this.list.innerHTML += `<li data-url="${arr[i].latest}"><a>${arr[i].name}</a></li>`;
 						}
-						this.list.parentElement.classList.add('open');
 						this.isShowingSuggestions = true;
+						if (!this.textareaBounds) {
+							this.textareaBounds = this.t.getBoundingClientRect();
+							this.list.style.top = this.textareaBounds.bottom + 'px';
+							this.list.style.left = this.textareaBounds.left + 'px';
+							this.list.style.width = this.textareaBounds.width + 'px';
+						}
+						this.list.classList.add('is-open');
 					});
 				}, 500);
 			}
 		}
 		onKeyDown(event) {
+			if (!this.isShowingSuggestions) { return; }
+
 			if (event.keyCode === 27) {
 				this.closeSuggestions();
 				event.stopPropagation();
@@ -109,6 +136,13 @@
 			} else if (event.keyCode === 13 && this.isShowingSuggestions) {
 				var selectedItemElement = this.list.querySelector('.selected');
 				this.replaceCurrentLine(selectedItemElement.dataset.url)
+				this.closeSuggestions();
+			}
+		}
+		onListMouseDown(event) {
+			var target = event.target;
+			if (target.parentElement.dataset.url) {
+				this.replaceCurrentLine(target.parentElement.dataset.url)
 				this.closeSuggestions();
 			}
 		}

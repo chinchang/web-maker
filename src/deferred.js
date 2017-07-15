abccf89b971e513b6099fd4326a061850c6593f2
Kushagra Gour <chinchang457@gmail.com>
2017-07-15 17:42:10 +0530

    deferred.js: add comments

--- src/deferred.js
+++ src/deferred.js
@@ -6,7 +6,9 @@
 			d.reject = reject;
 		});
 
+		// Add the native promise as a key on deferred object.
 		d.promise = promise;
+		// Also move all props/methods of native promise on the deferred obj.
 		return Object.assign(d, promise);
 	};
 })();

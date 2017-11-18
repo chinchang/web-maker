709c59f1b4a4c820cab0501eabd52b05cd7723d1
Kushagra Gour <chinchang457@gmail.com>
2017-11-19 02:09:49 +0530

    add build

--- dist/script.js
+++ dist/script.js
@@ -18,47 +18,52 @@
 'use strict';
 
 if ('serviceWorker' in navigator) {
-  // Delay registration until after the page has loaded, to ensure that our
-  // precaching requests don't degrade the first visit experience.
-  // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
-  window.addEventListener('load', function() {
-    // Your service-worker.js *must* be located at the top-level directory relative to your site.
-    // It won't be able to control pages unless it's located at the same level or higher than them.
-    // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-    // See https://github.com/slightlyoff/ServiceWorker/issues/468
-    navigator.serviceWorker.register('service-worker.js').then(function(reg) {
-      // updatefound is fired if service-worker.js changes.
-      reg.onupdatefound = function() {
-        // The updatefound event implies that reg.installing is set; see
-        // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
-        var installingWorker = reg.installing;
+	// Delay registration until after the page has loaded, to ensure that our
+	// precaching requests don't degrade the first visit experience.
+	// See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
+	window.addEventListener('load', function() {
+		// Your service-worker.js *must* be located at the top-level directory relative to your site.
+		// It won't be able to control pages unless it's located at the same level or higher than them.
+		// *Don't* register service worker file in, e.g., a scripts/ sub-directory!
+		// See https://github.com/slightlyoff/ServiceWorker/issues/468
+		navigator.serviceWorker
+			.register('service-worker.js')
+			.then(function(reg) {
+				// updatefound is fired if service-worker.js changes.
+				reg.onupdatefound = function() {
+					// The updatefound event implies that reg.installing is set; see
+					// https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
+					var installingWorker = reg.installing;
 
-        installingWorker.onstatechange = function() {
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                // At this point, the old content will have been purged and the fresh content will
-                // have been added to the cache.
-                // It's the perfect time to display a "New content is available; please refresh."
-                // message in the page's interface.
-                console.log('New or updated content is available.');
-              } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a "Content is cached for offline use." message.
-                console.log('Content is now available offline!');
-              }
-              break;
+					installingWorker.onstatechange = function() {
+						switch (installingWorker.state) {
+							case 'installed':
+								if (navigator.serviceWorker.controller) {
+									// At this point, the old content will have been purged and the fresh content will
+									// have been added to the cache.
+									// It's the perfect time to display a "New content is available; please refresh."
+									// message in the page's interface.
+									console.log('New or updated content is available.');
+								} else {
+									// At this point, everything has been precached.
+									// It's the perfect time to display a "Content is cached for offline use." message.
+									console.log('Content is now available offline!');
+								}
+								break;
 
-            case 'redundant':
-              console.error('The installing service worker became redundant.');
-              break;
-          }
-        };
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
-  });
+							case 'redundant':
+								console.error(
+									'The installing service worker became redundant.'
+								);
+								break;
+						}
+					};
+				};
+			})
+			.catch(function(e) {
+				console.error('Error during service worker registration:', e);
+			});
+	});
 }
 
 (function() {
@@ -1628,7 +1633,9 @@ globalConsoleContainerEl
 
 		contents +=
 			'<script src="' +
-			// chrome.extension.getURL('lib/screenlog.js') +
+			(chrome.extension
+				? chrome.extension.getURL('lib/screenlog.js')
+				: `${location.origin}/lib/screenlog.js`) +
 			'"></script>';
 
 		if (jsMode === JsModes.ES6) {
@@ -1638,7 +1645,7 @@ globalConsoleContainerEl
 				'"></script>';
 		}
 
-		if (js) {
+		if (js !== undefined) {
 			contents += '<script>\n' + js + '\n//# sourceURL=userscript.js';
 		} else {
 			var origin = chrome.i18n.getMessage()
@@ -1706,7 +1713,8 @@ globalConsoleContainerEl
 	}
 
 	function createPreviewFile(html, css, js) {
-		var contents = getCompleteHtml(html, css);
+		const shouldInlineJs = !window.webkitRequestFileSystem;
+		var contents = getCompleteHtml(html, css, shouldInlineJs ? js : '');
 		var blob = new Blob([contents], { type: 'text/plain;charset=UTF-8' });
 		var blobjs = new Blob([js], { type: 'text/plain;charset=UTF-8' });
 
@@ -1716,21 +1724,30 @@ globalConsoleContainerEl
 			trackEvent.hasTrackedCode = true;
 		}
 
-		// we need to store user script in external JS file to prevent inline-script
-		// CSP from affecting it.
-		writeFile('script.js', blobjs, function() {
-			writeFile('preview.html', blob, function() {
-				var origin = chrome.i18n.getMessage()
-					? `chrome-extension://${chrome.i18n.getMessage('@@extension_id')}`
-					: `${location.origin}`;
-				var src = `filesystem:${origin}/temporary/preview.html`;
-				if (scope.detachedWindow) {
-					scope.detachedWindow.postMessage(src, '*');
-				} else {
-					frame.src = src;
-				}
+		if (shouldInlineJs) {
+			frame.src = frame.src;
+			setTimeout(() => {
+				frame.contentDocument.open();
+				frame.contentDocument.write(contents);
+				frame.contentDocument.close();
+			}, 10);
+		} else {
+			// we need to store user script in external JS file to prevent inline-script
+			// CSP from affecting it.
+			writeFile('script.js', blobjs, function() {
+				writeFile('preview.html', blob, function() {
+					var origin = chrome.i18n.getMessage()
+						? `chrome-extension://${chrome.i18n.getMessage('@@extension_id')}`
+						: `${location.origin}`;
+					var src = `filesystem:${origin}/temporary/preview.html`;
+					if (scope.detachedWindow) {
+						scope.detachedWindow.postMessage(src, '*');
+					} else {
+						frame.src = src;
+					}
+				});
 			});
-		});
+		}
 	}
 
 	scope.setPreviewContent = function(isForced) {

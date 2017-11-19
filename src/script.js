90727ebd1620efb17fd57628bd54b66fba42815f
Kushagra Gour <chinchang457@gmail.com>
2017-11-19 23:47:02 +0530

    change build dir to app/

--- src/script.js
+++ src/script.js
@@ -101,7 +101,7 @@ globalConsoleContainerEl, externalLibrarySearchInput, keyboardShortcutsModal
 	};
 
 	const AUTO_SAVE_INTERVAL = 15000; // 15 seconds
-	const BASE_PATH = chrome.extension ? '/' : '/dist/';
+	const BASE_PATH = chrome.extension ? '/' : '/app/';
 
 	var updateTimer,
 		updateDelay = 500,

a56f1ae7cebdde65df5d539219293a38854351b0
Kushagra Gour <chinchang457@gmail.com>
2018-01-13 14:55:39 +0530

    prettier gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,33 @@ var gulp = require('gulp');
 var useref = require('gulp-useref');
 
 gulp.task('copyFiles', [], function() {
-	gulp.src('src/lib/codemirror/theme/*').pipe(gulp.dest('app/lib/codemirror/theme'));
-	gulp.src('src/lib/codemirror/mode/**/*').pipe(gulp.dest('app/lib/codemirror/mode'));
+	gulp
+		.src('src/lib/codemirror/theme/*')
+		.pipe(gulp.dest('app/lib/codemirror/theme'));
+	gulp
+		.src('src/lib/codemirror/mode/**/*')
+		.pipe(gulp.dest('app/lib/codemirror/mode'));
 	gulp.src('src/partials/*').pipe(gulp.dest('app/partials'));
 	gulp.src('src/lib/screenlog.js').pipe(gulp.dest('app/lib'));
 	gulp.src('src/icon-48.png').pipe(gulp.dest('app'));
-	gulp.src([ 'src/FiraCode.ttf', 'src/Fixedsys.ttf', 'src/Inconsolata.ttf', 'src/Monoid.ttf' ]).pipe(gulp.dest('app'));
+	gulp
+		.src([
+			'src/FiraCode.ttf',
+			'src/Fixedsys.ttf',
+			'src/Inconsolata.ttf',
+			'src/Monoid.ttf'
+		])
+		.pipe(gulp.dest('app'));
 });
 
-gulp.task('useRef', [ 'copyFiles' ], function() {
-	return gulp.src('src/index.html').pipe(useref()).pipe(gulp.dest('app'));
+gulp.task('useRef', ['copyFiles'], function() {
+	return gulp
+		.src('src/index.html')
+		.pipe(useref())
+		.pipe(gulp.dest('app'));
 });
 
-gulp.task('generate-service-worker', [ 'useRef' ], function(callback) {
+gulp.task('generate-service-worker', ['useRef'], function(callback) {
 	var swPrecache = require('sw-precache');
 	var rootDir = 'app';
 

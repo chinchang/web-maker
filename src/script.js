77414211f0e6df12e348a214da0beb6a1dac8332
Kushagra Gour <chinchang457@gmail.com>
2018-04-23 00:42:25 +0530

    bump to 3.1.1

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ loginModal, profileModal, profileAvatarImg, profileUserName, openItemsBtn, askTo
 (function(alertsService, itemService) {
 	/* eslint-enable no-extra-semi */
 	var scope = scope || {};
-	var version = '3.1.0';
+	var version = '3.1.1';
 
 	if (window.DEBUG) {
 		window.scope = scope;

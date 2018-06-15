6616fab11ca95af205b539d8d4b000f6cb0b8013
Kushagra Gour <chinchang457@gmail.com>
2018-06-15 17:30:28 +0530

    remove ported code from old files

--- src/script.js
+++ src/script.js
@@ -1,76 +1,31 @@
-/**
- * Compiles directives on the given node
- * @param {Node} root The element on which compilation is required
- */
-function compileNodes(root) {
-	if (!(root instanceof Node)) {
-		/* eslint-disable no-param-reassign */
-		root = document;
-		/* eslint-enable no-param-reassign */
-	}
-	// Create a querySelectorAll function bound to the passed `root` Node
-	const query = selector => [...root.querySelectorAll(selector)];
+// Console header drag resize logic
+var consoleHeaderDragStartY;
+var consoleInitialHeight;
 
-
-	// Compile d-open-modal directive
-	const modalTriggers = query(`[d-open-modal]`);
-	modalTriggers.forEach(function (el) {
-		utils.onButtonClick(el, function () {
-			scope.toggleModal(window[el.getAttribute('d-open-modal')]);
-			trackEvent(
-				el.getAttribute('data-event-category'),
-				el.getAttribute('data-event-action')
-			);
-		});
-	});
+function onConsoleHeaderDrag(e) {
+	consoleEl.style.height =
+		consoleInitialHeight + consoleHeaderDragStartY - e.pageY + 'px';
 }
+$('.js-console__header').addEventListener('mousedown', e => {
+	consoleHeaderDragStartY = e.pageY;
+	consoleInitialHeight = consoleEl.getBoundingClientRect().height;
+	$('#demo-frame').classList.add('pointer-none');
+	window.addEventListener('mousemove', onConsoleHeaderDrag);
+});
+$('.js-console__header').addEventListener('mouseup', () => {
+	window.removeEventListener('mousemove', onConsoleHeaderDrag);
+	$('#demo-frame').classList.remove('pointer-none');
+});
 
-function init() {
-	new TextareaAutoComplete(externalJsTextarea, {
-		filter: obj => obj.latest.match(/\.js$/)
-	});
-	new TextareaAutoComplete(externalCssTextarea, {
-		filter: obj => obj.latest.match(/\.css$/)
-	});
-	new TextareaAutoComplete(externalLibrarySearchInput, {
-		selectedCallback: value => {
-			const textarea = value.match(/\.js$/) ?
-				externalJsTextarea :
-				externalCssTextarea;
-			textarea.value = `${textarea.value}\n${value}`;
-			externalLibrarySearchInput.value = '';
+window.addEventListener('focusin', e => {
+	if (document.body.classList.contains('overlay-visible')) {
+		const modal = $('.is-modal-visible');
+		if (!modal) {
+			return;
 		}
-	});
-
-	// Console header drag resize logic
-	var consoleHeaderDragStartY;
-	var consoleInitialHeight;
-
-	function onConsoleHeaderDrag(e) {
-		consoleEl.style.height =
-			consoleInitialHeight + consoleHeaderDragStartY - e.pageY + 'px';
+		if (!modal.contains(e.target)) {
+			e.preventDefault();
+			modal.querySelector('.js-modal__close-btn').focus();
+		}
 	}
-	$('.js-console__header').addEventListener('mousedown', e => {
-		consoleHeaderDragStartY = e.pageY;
-		consoleInitialHeight = consoleEl.getBoundingClientRect().height;
-		$('#demo-frame').classList.add('pointer-none');
-		window.addEventListener('mousemove', onConsoleHeaderDrag);
-	});
-	$('.js-console__header').addEventListener('mouseup', () => {
-		window.removeEventListener('mousemove', onConsoleHeaderDrag);
-		$('#demo-frame').classList.remove('pointer-none');
-	});
-
-	window.addEventListener('focusin', e => {
-		if (document.body.classList.contains('overlay-visible')) {
-			const modal = $('.is-modal-visible');
-			if (!modal) {
-				return;
-			}
-			if (!modal.contains(e.target)) {
-				e.preventDefault();
-				modal.querySelector('.js-modal__close-btn').focus();
-			}
-		}
-	});
-}
+});

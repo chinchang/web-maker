2b8e773e034feeac1e53830d4610c6b8f187e959
Kushagra Gour <chinchang457@gmail.com>
2018-06-22 14:45:15 +0530

    remove assets, favicon, preload tags

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,4 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 /**
@@ -9,30 +9,35 @@ var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
  * @param {object} env - options passed to CLI.
  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
  **/
-export default function (config, env, helpers) {
+export default function(config, env, helpers) {
+	const htmlWebpackPlugin = helpers.getPluginsByName(
+		config,
+		'HtmlWebpackPlugin'
+	)[0];
+	Object.assign(htmlWebpackPlugin.plugin.options.minify, {
+		removeComments: false,
+		collapseWhitespace: false
+	});
+	htmlWebpackPlugin.plugin.options.preload = false;
+	htmlWebpackPlugin.plugin.options.favicon = false;
+
 	if (env.isProd) {
 		config.devtool = false; // disable sourcemaps
-
-		const htmlWebpackPlugin = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0];
-		Object.assign(htmlWebpackPlugin.plugin.options.minify, {
-			removeComments: false,
-			collapseWhitespace: false
-		})
 
 		config.plugins.push(
 			new CommonsChunkPlugin({
 				name: 'vendor',
-				minChunks: ({
-					resource
-				}) => /node_modules/.test(resource),
+				minChunks: ({ resource }) => /node_modules/.test(resource)
 			})
 		);
 
-		const swPlugin = helpers.getPluginsByName(config, 'SWPrecacheWebpackPlugin')[0];
-		config.plugins.splice(swPlugin.index, 1)
+		const swPlugin = helpers.getPluginsByName(
+			config,
+			'SWPrecacheWebpackPlugin'
+		)[0];
+		config.plugins.splice(swPlugin.index, 1);
 
-		const uglifyPlugin = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0]
-		config.plugins.splice(uglifyPlugin.index, 1)
+		const uglifyPlugin = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+		config.plugins.splice(uglifyPlugin.index, 1);
 	}
-
 }

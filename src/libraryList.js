1b58931c626c4b667d3c7eb6f820f69486af69fe
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2018-09-17 16:11:41 +0200

    Update libraryList.js
    upped
    
    Foundation - 6.5.0-rc3
    FontAwesome - 5.3.1.
    Uikit 3  - 3.0.0-rc.16

--- src/libraryList.js
+++ src/libraryList.js
@@ -17,7 +17,7 @@ export const jsLibs = [
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js',
+			'https://cdn.jsdelivr.net/npm/foundation-sites@6.5.0-rc.3/dist/js/foundation.min.js',
 		label: 'Foundation',
 		type: 'js'
 	},
@@ -54,7 +54,7 @@ export const jsLibs = [
 		type: 'js'
 	},
 	{
-		url: 'https://cdnjs.cloudflare.com/ajax/libs/d3/4.13.0/d3.min.js',
+		url: 'https://d3js.org/d3.v5.min.js',
 		label: 'D3',
 		type: 'js'
 	},
@@ -76,7 +76,7 @@ export const jsLibs = [
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.42/js/uikit.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.16/js/uikit.min.js',
 		label: 'UIkit 3',
 		type: 'js'
 	},
@@ -111,7 +111,7 @@ export const cssLibs = [
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css',
+			'https://cdn.jsdelivr.net/npm/foundation-sites@6.5.0-rc.3/dist/css/foundation.min.css',
 		label: 'Foundation',
 		type: 'css'
 	},
@@ -144,7 +144,7 @@ export const cssLibs = [
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.42/css/uikit.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.16/css/uikit.min.css',
 		label: 'UIkit 3',
 		type: 'css'
 	},
@@ -161,7 +161,7 @@ export const cssLibs = [
 		type: 'css'
 	},
 	{
-		url: 'https://use.fontawesome.com/releases/v5.0.10/css/all.css',
+		url: 'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
 		label: 'FontAwesome 5',
 		type: 'css'
 	}

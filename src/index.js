794cb5b321d2ec08ea87ddcfae792a9a561d455d
PengXiao <eagle.xiao@gmail.com>
2018-12-15 18:37:19 +1100

    Reverted Router in index.js .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import Router from 'preact-router';
-import { Component, render } from 'preact';
 import App from './components/app.jsx';
 
-export default class Main extends Component {
-	render() {
-		return <Router>
-                <App path="/" />
-               </Router>;
-	}
-}
+export default App;

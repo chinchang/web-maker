9b7cbd3a860817ef7bfcdc7ec9f6c4c87bbcb737
PengXiao <eagle.xiao@gmail.com>
2019-01-05 15:35:55 +1100

    Replaced the open up stage animation.

--- src/components/OnboardingModal.jsx
+++ src/components/OnboardingModal.jsx
@@ -1,19 +1,14 @@
-import { h } from 'preact';
 import Modal from './Modal.jsx';
 
 export function OnboardingModal(props) {
 	return (
 		<Modal show={props.show} closeHandler={props.closeHandler}>
 			<div class="tac">
-				<h1 style="margin-top:20px">Welcome to Web Sequence</h1>
+				<h1 style="margin-top:20px">Welcome to ZenUML - Web Sequence</h1>
 			</div>
-			<div class="flex" style="margin-top: 100px;">
-				<div class="onboard-step">
-					<img src="./animation/10s.gif" alt="Middleman logo" />
-				</div>
-
+			<div class="onboard-step">
+				<img src="./animation/open-up-stage.gif" alt="ZenUML animation" />
 			</div>
-
 		</Modal>
 	);
 }

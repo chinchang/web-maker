0591cf07e71b6d0a48289cce5e09cdb55e0cb02f
mwpeng <mingw.peng@gmail.com>
2019-02-21 21:21:31 +1100

    remove dot

--- src/zenuml/components/MainHeader/ProductVersionInfo/ProductVersionAnonymous.js
+++ src/zenuml/components/MainHeader/ProductVersionInfo/ProductVersionAnonymous.js
@@ -2,7 +2,7 @@ const ProductVersionAnonymous = () => {
 	return (
 		<i style="background-color:grey; border-radius: 4px;"
 			 className="flex  flex-v-center  hint--rounded  hint--bottom-left"
-			 aria-label="Please login to upgrade to Pro."
+			 aria-label="Please login to upgrade to Pro"
 		>
 			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pro&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		</i>

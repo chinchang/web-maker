af517d7d959013d369c62186ac35e8b6bf20c46d
Mingwei Peng <mingw.peng@gmail.com>
2019-02-22 10:43:10 +1100

    use padding instead of &nbsp;

--- src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAnonymous.js
+++ src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAnonymous.js
@@ -1,11 +1,9 @@
 const ProductVersionLabelAnonymous = () => {
 	return (
-		<i style="background-color:grey; border-radius: 4px;"
+		<i style="background-color:grey; border-radius: 4px; padding: 1px 15px 1px 15px;"
 			 className="flex  flex-v-center  hint--rounded  hint--bottom-left"
 			 aria-label="Please login to upgrade to Pro"
-		>
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pro&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		</i>
+		>Pro</i>
 	);
 };
 

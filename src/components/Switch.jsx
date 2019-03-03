2bc061f33b1c05a877ff06f55ab7725bb637763a
Kushagra Gour <chinchang457@gmail.com>
2019-03-03 12:59:15 +0530

    CreateNewModal: refactor ux

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,35 +1,43 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 
-export default class Switch extends Component {
-	render() {
-		return (
-			<label class="check-switch">
-				<input
-					role="switch"
-					type="checkbox"
-					checked={this.props.checked}
-					onChange={this.props.onChange}
-				/>
-				<div class="check-switch__toggle-wrap">
-					<span
-						aria-hidden="true"
-						class="check-switch__status"
-						style={`visibility:${this.props.checked ? 'hidden' : 'visible'}`}
-					>
-						Off
-					</span>
-					<span aria-hidden="true" class="check-switch__toggle" />
-					<span
-						aria-hidden="true"
-						class="check-switch__status"
-						style={`visibility:${this.props.checked ? 'visible' : 'hidden'}`}
-					>
-						On
-					</span>
-				</div>
+export default function Switch({
+	checked,
+	onChange,
+	labels,
+	children,
+	showBothLabels
+}) {
+	return (
+		<label class="check-switch">
+			<input
+				role="switch"
+				type="checkbox"
+				checked={checked}
+				onChange={onChange}
+			/>
+			<div class="check-switch__toggle-wrap">
+				<span
+					aria-hidden="true"
+					class="check-switch__status"
+					style={`visibility:${
+						checked && !showBothLabels ? 'hidden' : 'visible'
+					}`}
+				>
+					{labels[0] || 'Off'}
+				</span>
+				<span aria-hidden="true" class="check-switch__toggle" />
+				<span
+					aria-hidden="true"
+					class="check-switch__status"
+					style={`visibility:${
+						checked || showBothLabels ? 'visible' : 'hidden'
+					}`}
+				>
+					{labels[1] || 'On'}
+				</span>
+			</div>
 
-				{this.props.children}
-			</label>
-		);
-	}
+			{children}
+		</label>
+	);
 }

32c398564df8117fe83f157eaadd915886445403
Kushagra Gour <chinchang457@gmail.com>
2019-03-04 19:00:12 +0530

    contentwraps: remove unused code

--- src/components/ContentWrap.jsx
+++ src/components/ContentWrap.jsx
@@ -111,11 +111,7 @@ export default class ContentWrap extends Component {
 			// This is done so that multiple simultaneous setValue don't trigger too many preview refreshes
 			// and in turn too many file writes on a single file (eg. preview.html).
 			if (change.origin !== 'setValue') {
-				// Specifically checking for false so that the condition doesn't get true even
-				// on absent key - possible when the setting key hasn't been fetched yet.
-				if (this.prefs.autoPreview !== false) {
-					this.setPreviewContent();
-				}
+				this.setPreviewContent();
 
 				// Track when people actually are working.
 				trackEvent.previewCount = (trackEvent.previewCount || 0) + 1;

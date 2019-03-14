962021a8bfe20ec65745b076d11f8d8e8bda91fc
Kushagra Gour <chinchang457@gmail.com>
2019-03-14 23:32:10 +0530

    gulpfile: changes for new packages/website

--- gulpfile.js
+++ gulpfile.js
@@ -194,18 +194,14 @@ gulp.task('packageExtension', function() {
 	);
 });
 
+gulp.task('buildWebsite', function() {
+	return child_process.execSync('yarn run build-website');
+});
+
 gulp.task('buildDistFolder', function() {
-	child_process.execSync('mkdir dist');
-	child_process.execSync('cp index.html dist/');
-	child_process.execSync('cp -R uninstall dist/');
-	child_process.execSync('cp -R privacy-policy dist/');
-	child_process.execSync('cp -R icons dist/');
-	child_process.execSync('cp -R docs dist/');
-	child_process.execSync('cp -R chrome-logo.png dist/');
+	child_process.execSync('rm -rf dist');
+	child_process.execSync('mv packages/website/_site dist');
 	child_process.execSync('mv app dist/');
-	child_process.execSync('cp ss1.png dist/');
-	child_process.execSync('cp icon-128.png dist/');
-	child_process.execSync('cp manifest.json dist/');
 });
 
 gulp.task('cleanup', function() {
@@ -244,7 +240,7 @@ gulp.task('release', function(callback) {
 
 gulp.task('dev-release', function(callback) {
 	runSequence(
-		'runWebpack',
+		['runWebpack', 'buildWebsite'],
 		'copyFiles',
 		'fixIndex',
 		'useRef',

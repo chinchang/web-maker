e16b6795f153d86fe4f59323093f747c93769704
Kushagra Gour <chinchang457@gmail.com>
2019-06-26 17:40:46 +0530

    bump to 4.0.2

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -77,7 +77,7 @@ const LocalStorageKeys = {
 	ASKED_TO_IMPORT_CREATIONS: 'askedToImportCreations'
 };
 const UNSAVED_WARNING_COUNT = 15;
-const version = '4.0.1';
+const version = '4.0.2';
 
 // Read forced settings as query parameters
 window.forcedSettings = {};

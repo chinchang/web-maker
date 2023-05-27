607e63f123a028e5b3a007f81ea565b53079622a
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2023-05-27 22:52:47 +0200

    Update libraryList.js

--- src/libraryList.js
+++ src/libraryList.js
@@ -1,12 +1,12 @@
 export const jsLibs = [
 	{
-		url: 'https://code.jquery.com/jquery-3.3.1.min.js',
+		url: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js',
 		label: 'jQuery',
 		type: 'js'
 	},
 	{
-		url: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
-		label: 'Bootstrap 3',
+		url: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js',
+		label: 'Bootstrap 5',
 		type: 'js'
 	},
 	{
@@ -17,7 +17,7 @@ export const jsLibs = [
 	},
 	{
 		url:
-			'https://cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/js/foundation.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.7.5/js/foundation.min.js',
 		label: 'Foundation',
 		type: 'js'
 	},
@@ -33,19 +33,19 @@ export const jsLibs = [
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/react/16.8.4/umd/react.production.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
 		label: 'React',
 		type: 'js'
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.8.4/umd/react-dom.production.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
 		label: 'React DOM',
 		type: 'js'
 	},
 	{
-		url: 'https://unpkg.com/vue/dist/vue.min.js',
-		label: 'Vue.js',
+		url: 'https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js',
+		label: 'Vue 2.js',
 		type: 'js'
 	},
 	{
@@ -74,13 +74,7 @@ export const jsLibs = [
 		type: 'js'
 	},
 	{
-		url: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.5/js/uikit.min.js',
-		label: 'UIkit 2',
-		type: 'js'
-	},
-	{
-		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/js/uikit.min.js',
+		url:'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.16.19/js/uikit.min.js',
 		label: 'UIkit 3',
 		type: 'js'
 	},
@@ -110,8 +104,8 @@ export const jsLibs = [
 export const cssLibs = [
 	{
 		url:
-			'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-		label: 'Bootstrap 3',
+			'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css',
+		label: 'Bootstrap 5',
 		type: 'css'
 	},
 	{
@@ -122,7 +116,7 @@ export const cssLibs = [
 	},
 	{
 		url:
-			'https://cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/css/foundation.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.7.5/css/foundation.min.css',
 		label: 'Foundation',
 		type: 'css'
 	},
@@ -132,36 +126,36 @@ export const cssLibs = [
 		type: 'css'
 	},
 	{
-		url: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css',
+		url: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css',
 		label: 'Bulma',
 		type: 'css'
 	},
 
 	{
-		url: 'https://cdnjs.cloudflare.com/ajax/libs/hint.css/2.5.0/hint.min.css',
+		url: 'https://cdnjs.cloudflare.com/ajax/libs/hint.css/2.7.0/hint.min.css',
 		label: 'Hint.css',
 		type: 'css'
 	},
 	{
 		url: 'https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css',
-		label: 'Tailwind.css',
+		label: 'Tailwind 2',
 		type: 'css'
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.5/css/uikit.min.css',
-		label: 'UIkit 2',
+			'https://cdn.tailwindcss.com/3.3.1',
+		label: 'Tailwind 3',
 		type: 'css'
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.5/css/uikit.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.16.19/css/uikit.min.css',
 		label: 'UIkit 3',
 		type: 'css'
 	},
 	{
 		url:
-			'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
 		label: 'Animate.css',
 		type: 'css'
 	},

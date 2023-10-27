dafda3ffa6353f6eca02ec4e0c52f221714f9352
Kushagra Gour <chinchang457@gmail.com>
2023-10-27 15:13:45 +0530

    bump to 5.1.1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -75,7 +75,7 @@ if (module.hot) {
 }
 
 const UNSAVED_WARNING_COUNT = 15;
-const version = '5.1.0';
+const version = '5.1.1';
 
 // Read forced settings as query parameters
 window.forcedSettings = {};

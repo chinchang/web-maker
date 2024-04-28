05c01d15b6d0c29a701573df2ca320d094cf6b35
Kushagra Gour <chinchang457@gmail.com>
2024-04-28 21:57:30 +0530

    fix template imgs

--- src/templateList.js
+++ src/templateList.js
@@ -2,18 +2,18 @@ export default [
 	{
 		id: 'react',
 		title: 'React',
-		img: '/assets/react-logo.svg'
+		img: 'assets/react-logo.svg'
 	},
 	{
 		id: 'vue',
 		title: 'Vue',
-		img: '/assets/vue-logo.svg',
+		img: 'assets/vue-logo.svg',
 		isFileModeSupported: true
 	},
 	{
 		id: 'preact',
 		title: 'Preact',
-		img: '/assets/preact-logo.svg'
+		img: 'assets/preact-logo.svg'
 	},
 	{
 		id: 'tailwind2',
@@ -28,7 +28,7 @@ export default [
 	{
 		id: 'kontra-game-engine',
 		title: 'Kontra Game Engine',
-		img: '/assets/html5-logo.svg',
+		img: 'assets/html5-logo.svg',
 		isFileModeSupported: true
 	}
 ];

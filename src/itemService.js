3d0f5ba6d92572f7c5681b1bb85d15f6bb19a8da
Kushagra Gour <chinchang457@gmail.com>
2024-05-07 13:19:33 +0530

    make item fetching from db independent of user.items

--- src/itemService.js
+++ src/itemService.js
@@ -37,13 +37,7 @@ export const itemService = {
 	async getAllItems(shouldFetchLocally) {
 		var t = Date.now();
 		var d = deferred();
-		let itemIds = await this.getUserItemIds(shouldFetchLocally);
-		itemIds = Object.getOwnPropertyNames(itemIds || {});
-		// log('itemids', itemIds);
 
-		if (!itemIds.length) {
-			d.resolve([]);
-		}
 		const items = [];
 
 		if (window.user && !shouldFetchLocally) {
@@ -65,6 +59,13 @@ export const itemService = {
 					}
 				);
 		} else {
+			let itemIds = await this.getUserItemIds(shouldFetchLocally);
+			itemIds = Object.getOwnPropertyNames(itemIds || {});
+
+			if (!itemIds.length) {
+				d.resolve([]);
+			}
+
 			for (let i = 0; i < itemIds.length; i++) {
 				/* eslint-disable no-loop-func */
 				window.db.local.get(itemIds[i], itemResult => {

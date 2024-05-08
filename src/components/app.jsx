636d2de5dc76356eac0c62b55b7492b70c7cbb27
Kushagra Gour <chinchang457@gmail.com>
2024-05-08 13:37:06 +0530

    bump to 6.2.0

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -84,7 +84,7 @@ if (module.hot) {
 }
 
 const UNSAVED_WARNING_COUNT = 15;
-const version = '6.1.0';
+const version = '6.2.0';
 
 // Read forced settings as query parameters
 window.forcedSettings = {};
